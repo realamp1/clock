96f003df7065fc9f6e419cb3e589031a0d0f44e4
Christian Larsen <christi9nl9rsen@gmail.com>
2024-04-02 08:08:56 -0500

    formatting

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ function getTime() {
     let hours = currentDate.getHours();
     const minutes = currentDate.getMinutes();
     const am_pm = hours >= 12 ? 'PM' : 'AM';
-    
-    // Convert hours to 12-hour format
     if (hours > 12) {
         hours -= 12;
     } else if (hours === 0) {
         hours = 12;
     }
-
     const formattedTime = `${hours}:${minutes < 10 ? '0' + minutes : minutes} ${am_pm}`;
     document.getElementById("time").textContent = formattedTime;
 }
